e33550a0bffb73df741b3db546a685f1683fb18b
Mc Genrev Egar <megar.global@megaworldcorp.com>
2026-07-27 08:35:39 +0800

    incident reporting system UBEA version

---
+++ script.js
@@ -0,0 +1,14 @@
+function handleClick(type) {
+  if (type === 'initial') {
+    alert("Initial Report Clicked");
+    // window.location.href = "initial.html";
+  } 
+  else if (type === 'progress') {
+    alert("Progress Report Clicked");
+    // window.location.href = "progress.html";
+  } 
+  else if (type === 'info') {
+    alert("Information Report Clicked");
+    // window.location.href = "info.html";
+  }
+}
